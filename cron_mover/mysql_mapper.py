22a22a48e24f56ae78b58e4179f9c8e9d0a4e657
sierra-95 <michaelmachohi@gmail.com>
2023-10-26 18:35:53 +0300

    message

--- cron_mover/mysql_mapper.py
+++ cron_mover/mysql_mapper.py
@@ -1,46 +1,38 @@
 import mysql.connector
 from datetime import datetime
+import json
 
-# MySQL connection configuration
 db_config = {
-    "host": "localhost",  # Use the host where your MySQL database is running
-    "user": "ubuntu",     # Your MySQL username
-    "password": "Michael@92!7", # Your MySQL password
-    "database": "sensor_data"  # Your MySQL database name
+    "host": "localhost",  
+    "user": "ubuntu",   
+    "password": "Michael@92!7", 
+    "database": "sensor_data" 
 }
-
-# Function to read data from a file and insert it into the database
 def insert_data_from_file(filename, table_name):
     try:
         with open(filename, "r") as file:
             for line in file:
-                data = line.strip()  # Read each line from the file
-                # Split the data and format as needed for your specific file structure
-                # For example, if your humidity.txt and temperature.txt have different structures,
-                # you may need to adapt this part accordingly.
-                if table_name == "humidity_data":
-                    timestamp, humidity = data.split(":")
-                    timestamp = datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S")
-                    cursor.execute(f"INSERT INTO {table_name} (timestamp, humidity) VALUES (%s, %s)",
-                                   (timestamp, humidity))
-                elif table_name == "temperature_data":
-                    # Handle temperature data format if different from humidity
-                    pass
-                elif table_name == "rfid_data":
-                    # Handle RFID data format if different
-                    pass
+                data = line.strip()
+                try:
+                    json_data = json.loads(data)  
+                    if table_name == "humidity_data":
+                        humidity = json_data["Humidity"]
+                        cursor.execute(f"INSERT INTO {table_name} (humidity) VALUES (%s)", (humidity,))
+                    elif table_name == "temperature_data":
+                        pass
+                    elif table_name == "rfid_data":
+                        pass
+                except json.JSONDecodeError:
+                    print(f"Error parsing JSON data from {filename}.")
     except FileNotFoundError:
         print(f"File {filename} not found.")
 
-# Establish a connection to the MySQL database
 db_connection = mysql.connector.connect(**db_config)
 cursor = db_connection.cursor()
 
-# Call the function to insert data from each file into the corresponding table
 insert_data_from_file("humidity.txt", "humidity_data")
 insert_data_from_file("temperature.txt", "temperature_data")
-insert_data_from_file("rfid.txt", "rfid_data")
+insert_data_from_file("login.txt", "rfid_data")
 
-# Commit the changes and close the connection
 db_connection.commit()
 db_connection.close()
--- cron_mover/transport_files.sh
+++ cron_mover/transport_files.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-HUMIDITY_FILE="/path/to/your/files/humidity.txt"
-LOGIN_FILE="/path/to/your/files/login.txt"
-RFID_FILE="/path/to/your/files/rfid.txt"
+HUMIDITY_FILE="/home/sierra-95/sensor-logger/humidity.txt"
+LOGIN_FILE="/home/sierra-95/sensor-logger/login.txt"
+RFID_FILE="/home/sierra-95/sensor-logger/temperature.txt"
 
 SSH_OPTIONS="-i ~/.ssh/id_rsa -o StrictHostKeyChecking=no"
 
 SERVER_USER="ubuntu"
 SERVER_IP="174.129.54.202"
-DESTINATION_DIR="/path/on/server/"
+DESTINATION_DIR="~/sensor-logger/cron_mover/"
 
 scp $SSH_OPTIONS $HUMIDITY_FILE $SERVER_USER@$SERVER_IP:$DESTINATION_DIR
 scp $SSH_OPTIONS $LOGIN_FILE $SERVER_USER@$SERVER_IP:$DESTINATION_DIR
