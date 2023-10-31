99384ee13f31a7391edce888e71fc10be64b5bdd
sierra-95 <michaelmachohi@gmail.com>
2023-10-31 18:12:42 +0300

    message

--- web_flask/static.py
+++ web_flask/static.py
@@ -4,23 +4,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('/var/www/html/index.html')
 
 @app.route('/humidity')
 def humidity():
-    return render_template('humidity.html')
+    return render_template('/var/www/html/humidity.html')
 
 @app.route('/temperature')
 def temperature():
-    return render_template('temperature.html')
+    return render_template('/var/www/html/temperature.html')
 
 @app.route('/data_visualization')
 def data_visualization():
-    return render_template('data_visualization.html')
+    return render_template('/var/www/html/data_visualization.html')
 
 @app.route('/login')
 def login():
-    return render_template('login.html')
+    return render_template('/var/www/html/login.html')
 
 if __name__ == '__main':
     app.run(host='0.0.0.0', port='5000')
