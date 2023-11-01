1dea24a55cc3e00d113e16be81b94385c915bd5d
sierra-95 <michaelmachohi@gmail.com>
2023-11-01 13:10:07 +0300

    message

---
+++ sensor-logger-api/app.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const mysql = require('mysql');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// MySQL database configuration
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'ubuntu',
+  password: 'Michael@92!7',
+  database: 'sensor_data',
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error('Database connection error: ' + err.message);
+  } else {
+    console.log('Database connection is active.');
+  }
+});
+
+// Define API endpoints
+app.get('/api/humidity', (req, res) => {
+  db.query('SELECT * FROM humidity_data', (error, results) => {
+    if (error) {
+      res.status(500).json({ error: 'Database error' });
+    } else {
+      res.status(200).json(results);
+    }
+  });
+});
+
+app.get('/api/rfid', (req, res) => {
+  db.query('SELECT * FROM rfid_data', (error, results) => {
+    if (error) {
+      res.status(500).json({ error: 'Database error' });
+    } else {
+      res.status(200).json(results);
+    }
+  });
+});
+
+app.get('/api/temperature', (req, res) => {
+  db.query('SELECT * FROM temperature_data', (error, results) => {
+    if (error) {
+      res.status(500).json({ error: 'Database error' });
+    } else {
+      res.status(200).json(results);
+    }
+  });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`API server is running on port ${port}`);
+});
