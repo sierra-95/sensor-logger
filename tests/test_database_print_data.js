bfcb84c87107441baa8b2d908678c95aa56b4051
sierra-95 <michaelmachohi@gmail.com>
2023-11-06 18:09:18 +0300

    message

---
+++ tests/test_database_print_data.js
@@ -0,0 +1,30 @@
+const mysql = require('mysql');
+
+// MySQL database configuration
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'ubuntu',
+  password: 'Michael@92!7',
+  database: 'sensor_data',
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error('Database connection error: ' + err.message);
+  } else {
+    console.log('Database connection is active.');
+
+    // Query the rfid_data table
+    db.query('SELECT * FROM rfid_data', (error, results) => {
+      if (error) {
+        console.error('Error querying the database: ' + error);
+      } else {
+        console.log('RFID Data:');
+        console.table(results);
+      }
+
+      // Close the database connection
+      db.end();
+    });
+  }
+});
